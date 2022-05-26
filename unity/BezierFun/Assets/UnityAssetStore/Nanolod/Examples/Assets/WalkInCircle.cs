fc7410f449e757d631f765ce14e5d159f74abebb
Jonas Aklin <jonas.aklin@faigle.ch>
2022-05-26 18:36:36 +0200

    Cleanup

--- unity/BezierFun/Assets/UnityAssetStore/Nanolod/Examples/Assets/WalkInCircle.cs
+++ unity/BezierFun/Assets/UnityAssetStore/Nanolod/Examples/Assets/WalkInCircle.cs
@@ -1,20 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WalkInCircle : MonoBehaviour
 {
     public float radius = 1f;
     public float speed = 100f;
-
+    
     private float angle = 0;
-
-    private void FixedUpdate()
+    
+    void FixedUpdate ()
     {
-        var x = this.radius * Mathf.Cos(this.angle * Mathf.Deg2Rad);
-        var y = this.radius * Mathf.Sin(this.angle * Mathf.Deg2Rad);
+        float x = radius * Mathf.Cos(angle * Mathf.Deg2Rad);
+        float y = radius * Mathf.Sin(angle * Mathf.Deg2Rad);
 
-        this.transform.localRotation = Quaternion.Euler(0, 0 - this.angle, 0);
-        this.transform.localPosition = new Vector3(x, this.transform.localPosition.y, y);
-
-        this.angle += this.speed * Time.deltaTime / this.radius;
+        transform.localRotation = Quaternion.Euler(0, 0 - angle, 0);
+        transform.localPosition = new Vector3(x, transform.localPosition.y, y);
+ 
+        angle += speed * Time.deltaTime / radius;
     }
 }
--- unity/BezierFun/Assets/UnityAssetStore/Nanolod/Examples/Runtime/DecimateSlider.cs
+++ unity/BezierFun/Assets/UnityAssetStore/Nanolod/Examples/Runtime/DecimateSlider.cs
@@ -1,8 +1,8 @@
-using Nanolod;
-using Nanomesh;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Nanolod;
+using Nanomesh;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,7 +12,7 @@ public class DecimateSlider : MonoBehaviour
     public GameObject original;
 
     public Text polycountLabel;
-
+    
     private Slider _slider;
     private List<Mesh> _originalMeshes;
     private List<Mesh> _meshes;
@@ -20,70 +20,64 @@ public class DecimateSlider : MonoBehaviour
 
     private void Start()
     {
-        this._originalMeshes = new List<Mesh>();
-        this._meshes = new List<Mesh>();
+        _originalMeshes = new List<Mesh>();
+        _meshes = new List<Mesh>();
 
-        this._slider = this.GetComponent<Slider>();
-        this._slider.onValueChanged.AddListener(this.OnValueChanged);
+        _slider = GetComponent<Slider>();
+        _slider.onValueChanged.AddListener(OnValueChanged);
 
-        var renderers = this.original.GetComponentsInChildren<Renderer>();
+        Renderer[] renderers = original.GetComponentsInChildren<Renderer>();
 
-        var uniqueMeshes = new HashSet<Mesh>();
+        HashSet<Mesh> uniqueMeshes = new HashSet<Mesh>();
 
-        foreach (var renderer in renderers)
+        foreach (Renderer renderer in renderers)
         {
             if (renderer is MeshRenderer meshRenderer)
             {
                 var meshFilter = renderer.gameObject.GetComponent<MeshFilter>();
                 if (!meshFilter)
-                {
                     continue;
-                }
-
+                
                 var mesh = meshFilter.sharedMesh;
 
-                this._polycount += mesh.triangles.Length;
-
+                _polycount += mesh.triangles.Length;
+                
                 if (!uniqueMeshes.Add(mesh))
-                {
                     continue;
-                }
-
-                this._originalMeshes.Add(mesh);
-
+                
+                _originalMeshes.Add(mesh);
+                
                 // Clone mesh
                 mesh = meshFilter.sharedMesh = UnityConverter.ToSharedMesh(mesh).ToUnityMesh();
-                this._meshes.Add(mesh);
+                _meshes.Add(mesh);
             }
             else if (renderer is SkinnedMeshRenderer skinnedMeshRenderer)
             {
                 var mesh = skinnedMeshRenderer.sharedMesh;
-
-                this._polycount += mesh.triangles.Length;
-
+                
+                _polycount += mesh.triangles.Length;
+                
                 if (!uniqueMeshes.Add(mesh))
-                {
                     continue;
-                }
-
-                this._originalMeshes.Add(mesh);
-
+                
+                _originalMeshes.Add(mesh);
+                
                 // Clone mesh
                 mesh = skinnedMeshRenderer.sharedMesh = mesh.ToSharedMesh().ToUnityMesh();
-                this._meshes.Add(mesh);
+                _meshes.Add(mesh);
             }
         }
 
-        this.OnValueChanged(1);
+        OnValueChanged(1);
     }
 
     private void OnValueChanged(float value)
     {
-        this.polycountLabel.text = $"{Math.Round(100 * value)}% ({Math.Round(value * this._polycount)}/{this._polycount} triangles)";
-
+        polycountLabel.text = $"{Math.Round(100 * value)}% ({Math.Round(value * _polycount)}/{_polycount} triangles)";
+        
         Profiling.Start("Convert");
 
-        var connectedMeshes = this._originalMeshes.Select(x => UnityConverter.ToSharedMesh(x).ToConnectedMesh()).ToArray();
+        var connectedMeshes = _originalMeshes.Select(x => UnityConverter.ToSharedMesh(x).ToConnectedMesh()).ToArray();
 
         Debug.Log(Profiling.End("Convert"));
         Profiling.Start("Clean");
@@ -101,7 +95,7 @@ public class DecimateSlider : MonoBehaviour
         Debug.Log(Profiling.End("Clean"));
         Profiling.Start("Decimate");
 
-        var sceneDecimator = new SceneDecimator();
+        SceneDecimator sceneDecimator = new SceneDecimator();
         sceneDecimator.Initialize(connectedMeshes);
 
         sceneDecimator.DecimateToRatio(value);
@@ -109,11 +103,11 @@ public class DecimateSlider : MonoBehaviour
         Debug.Log(Profiling.End("Decimate"));
         Profiling.Start("Convert back");
 
-        for (var i = 0; i < connectedMeshes.Length; i++)
+        for (int i = 0; i < connectedMeshes.Length; i++)
         {
-            this._meshes[i].Clear();
-            connectedMeshes[i].ToSharedMesh().ToUnityMesh(this._meshes[i]);
-            this._meshes[i].bindposes = this._originalMeshes[i].bindposes;
+            _meshes[i].Clear();
+            connectedMeshes[i].ToSharedMesh().ToUnityMesh(_meshes[i]);
+            _meshes[i].bindposes = _originalMeshes[i].bindposes;
         }
 
         Debug.Log(Profiling.End("Convert back"));
